7f8ba97cb960e48f8016d52e1d4460ac1f873432
smokris <smokris@softpixel.com>
2018-04-02 11:46:42 -0400

    Update LLVM package.

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class RtMidiConan(ConanFile):
     package_version = '2'
     version = '%s-%s' % (source_version, package_version)
 
-    requires = 'llvm/3.3-1@vuo/stable',
+    requires = 'llvm/3.3-2@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'http://www.music.mcgill.ca/~gary/rtmidi/'
     license = 'http://www.music.mcgill.ca/~gary/rtmidi/#license'
